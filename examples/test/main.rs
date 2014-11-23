f028da57fb51f1831126e7aad5f8d29a4edb1c04
Kevin Mehall <km@kevinmehall.net>
2014-11-23 13:32:46 -0800

    Update examples/test to semi-modern Rust

--- examples/test/main.rs
+++ examples/test/main.rs
@@ -1,15 +1,15 @@
-extern mod usb;
+extern crate usb;
 use usb::libusb;
 
 fn main() {
 	let c = usb::Context::new();
-	c.setDebug(2);
-	
-	let devices = c.listDevices();
+	c.set_debug(2);
+
+	let devices = c.list_devices();
 
 	for dev in devices.iter() {
 		let desc = dev.descriptor();
-		println!("Device {:i}.{:i} {:04x}:{:04x}",
+		println!("Device {:03}.{:03} {:04x}:{:04x}",
 			dev.bus(),
 			dev.address(),
 			desc.idVendor as uint,
@@ -24,41 +24,41 @@ fn main() {
 					let handle1 = handle.clone();
 					let handle2 = handle.clone();
 
-					do spawn {
+					std::task::spawn(proc() {
 						println!("1 Opened device {}", handle1.ptr());
-						println!("ctrl {:?}", handle1.ctrl_read(0xC0, 0x20, 0, 0, 64));
-						println!("Write {:?}", handle1.write(0x02, libusb::LIBUSB_TRANSFER_TYPE_BULK, [1,2,3]));
-						do handle1.write_stream(0x02, libusb::LIBUSB_TRANSFER_TYPE_BULK, 640, 8) |r| {
-							match (r) {
+						println!("ctrl {}", handle1.ctrl_read(0xC0, 0x20, 0, 0, 64));
+						println!("Write {}", handle1.write(0x02, libusb::LIBUSB_TRANSFER_TYPE_BULK, &[1,2,3]));
+						handle1.write_stream(0x02, libusb::LIBUSB_TRANSFER_TYPE_BULK, 640, 8, |r| {
+							match r {
 								Ok(buf) => {
-									println("Write OK");
+									println!("Write OK");
 									buf[0] = 5;
 								},
 								Err(code) => {
-									println!("Write error {:?}", code);
+									println!("Write error {}", code);
 								}
 							}
 							true
-						}
+						});
 
-					}
-					do spawn {
-						println!("2 Opened device {:?}", handle2.ptr());
-						println!("Read {:?}", handle2.read(0x81, libusb::LIBUSB_TRANSFER_TYPE_BULK, 64));
-						do handle2.read_stream(0x81, libusb::LIBUSB_TRANSFER_TYPE_BULK, 640, 8) |r| {
-							match (r) {
-								Ok(buf) => println!("Read {:?}", buf.slice(0, 10)),
-								Err(code) => println!("Read error {:?}", code)
+					});
+					std::task::spawn(proc() {
+						println!("2 Opened device {}", handle2.ptr());
+						println!("Read {}", handle2.read(0x81, libusb::LIBUSB_TRANSFER_TYPE_BULK, 64));
+						handle2.read_stream(0x81, libusb::LIBUSB_TRANSFER_TYPE_BULK, 640, 8, |r| {
+							match r {
+								Ok(buf) => println!("Read {}", buf.slice(0, 10)),
+								Err(code) => println!("Read error {}", code)
 							}
 							true
-						}
-					}
+						});
+					});
 				},
 				Err(code) => {
-					println!("Error opening device: {:?}", code);
+					println!("Error opening device: {}", code);
 				}
 			}
 		},
-		None => println("Device not found"),
+		None => println!("Device not found"),
 	}
 }
