a944ac90fdfd3a5325f702995d4b4fa358ba2018
Kevin Mehall <km@kevinmehall.net>
2025-03-02 16:27:27 -0700

    Set edition

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 #![no_std]
-//! Common definitions for USB constants.
+//! Common definitions for USB constants from the USB specification:
 //!
 //! * [Universal Serial Bus Specification Revision 2.0](https://www.usb.org/document-library/usb-20-specification)
 //! * [Universal Serial Bus 3.2 Specification](https://usb.org/document-library/usb-32-revision-11-june-2022)
 
 mod descriptor;
-pub use descriptor::{class_code, descriptor_type, language_id};
+pub use self::descriptor::{class_code, descriptor_type, language_id};
 
 mod endpoint;
-pub use endpoint::{endpoint_address, endpoint_attributes};
+pub use self::endpoint::{endpoint_address, endpoint_attributes};
 
 mod request;
-pub use request::{feature_selector, request_type, standard_request, test_mode};
+pub use self::request::{feature_selector, request_type, standard_request, test_mode};
 
 mod bos;
-pub use bos::capability_type;
+pub use self::bos::capability_type;
